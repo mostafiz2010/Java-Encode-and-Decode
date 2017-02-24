c4e33ff62d66cfc77e0fe726266704668083d622
mostafiz2010 <mostafiz.de@gmail.com>
2017-02-24 11:51:27 +0100

    Update EncodeCheck1.java

--- EncodeCheck1.java
+++ EncodeCheck1.java
@@ -13,26 +13,19 @@ import java.util.List;
 
 public class EncodingAndDecodingUseToCheckFileAndFolderName {
 	
-	
 	public static List<String> scanFileFolderNameEncoding(String dataFolderPathName) throws Exception{
 		
 		List<String> wrongEncodingList = new ArrayList<String>();
-		
 		File dataFolder = new File(dataFolderPathName);
-		
 		wrongEncodingList = pullDirectoryContents(dataFolder, wrongEncodingList);
-		
 		return wrongEncodingList;
 	}
 	
-
 	private static List<String> pullDirectoryContents(File dataFolder, List<String> wrongEncodingList) throws IOException{
 		File [] files = dataFolder.listFiles();
-		
 		if(null == files){
 			return wrongEncodingList;
 		}
-		
 		for(File child : dataFolder.listFiles()){
 			if(!encodingUTF8(child.getName())){
 				wrongEncodingList.add(child.getAbsolutePath());
@@ -60,11 +53,9 @@ public class EncodingAndDecodingUseToCheckFileAndFolderName {
 	//	selectedFolder = URLEncoder.encode(selectedFolder, "ISO-8859-1"); // H%C3%A9l%C3%A8ne
 	//	selectedFolder = URLDecoder.decode(selectedFolder, "UTF-8"); // and finally : testüß
 	//
-	
-	
 
 	public static void main (String ...args) throws Exception{
-		List<String> wrongEncodingList = scanFileFolderNameEncoding("/Users/mostafizur/Desktop/test");
+		List<String> wrongEncodingList = scanFileFolderNameEncoding("/Users/mr/Desktop/test");
 		for(String eco : wrongEncodingList){
 			System.out.println(eco.toString());
 		}
